867e79c52cff2adea6131c9f9e14a8f2392ecb84
Param Prajapati <param@MacBookAir-102.lan>
2025-01-21 01:45:22 +0530

    first commit

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import models
 from blocklist import BLOCKLIST
 from flask_jwt_extended import JWTManager
 from db import db
+import os 
+
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
@@ -21,7 +23,7 @@ def create_app(db_url=None):
     app.config[
         "OPENAPI_SWAGGER_UI_URL"
     ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE_EXCEPTIONS"] = True
     db.init_app(app)
