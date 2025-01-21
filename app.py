c66f04474dd5d5dc4ae092359bd6324cab57173e
Param Prajapati <param@Params-MacBook-Air.local>
2025-01-21 11:17:33 +0530

    changed db

--- app.py
+++ app.py
@@ -100,6 +100,11 @@ def create_app(db_url=None):
             401,
         )
     
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        
+
     # with app.app_context():
     #     db.create_all()
 
